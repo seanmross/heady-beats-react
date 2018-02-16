be58e49d4ee5c5858fb90ad0cfa0a5c91bba7d88
Sean Ross <seanmross9@gmail.com>
2018-02-15 22:26:55 -0500

    added reactstrap styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Landing from './Landing'
 import Navigation from './Navigation'
-// Theme
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
-import grey from 'material-ui/colors/grey'
-
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: grey[700],
-      main: grey[800],
-      dark: grey[900],
-      contrastText: grey[50],
-    },
-  },
-  overrides: {
-    MuiButton: {
-      root: {
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      },
-    },
-    MuiIconButton: {
-      root: {
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      },
-    },
-  },
-})
 
 class App extends Component {
   render() {
     return (
       <div>
-        <MuiThemeProvider theme={theme} >
-          <Navigation />
-          <Landing />
-        </MuiThemeProvider>
+        <Navigation />
+        <Landing />
       </div>
-      
     )
   }
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,43 +1,39 @@
-import React from 'react'
-import AppBar from 'material-ui/AppBar'
-import Toolbar from 'material-ui/Toolbar'
-import Typography from 'material-ui/Typography'
-import IconButton from 'material-ui/IconButton'
-import MenuIcon from 'material-ui-icons/Menu'
-import Button from 'material-ui/Button'
-import { withStyles } from 'material-ui/styles'
-import logo from '../assets/images/logo.png'
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-const styles = {
-    appBar: {
-        width: '100%',
-    },
-    pullRight: {
-        flex: 1,
-    },
-    menuButton: {
-        marginLeft: -12,
-        marginRight: 20,
-    },
-};
+export default class Example extends React.Component {
+    constructor(props) {
+        super(props);
 
-function Navigation(props){
-    const { classes } = props;
-    return(
-        <div className={classes.appBar}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                        <img src={logo} />
-                    </IconButton>
-                    <Typography variant="headline" color="inherit" className={classes.pullRight}>
-                        headybeats
-                    </Typography>
-                    <Button color="inherit" background-color="primary.light">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true
+        };
+    }
+
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="faded" light>
+                    <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                    <Collapse isOpen={!this.state.collapsed} navbar>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink href="/components/">Components</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
 }
-
-export default withStyles(styles)(Navigation)
