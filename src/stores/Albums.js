db6fdc1020e2f784b74df9c4df6cd622403baee4
Sean Ross <seanmross9@gmail.com>
2018-02-18 21:11:00 -0500

    album store data update

--- src/stores/Albums.js
+++ src/stores/Albums.js
@@ -3,7 +3,7 @@ import { observable } from 'mobx'
 class AlbumStore {
     @observable albums = [
         { id: 1, albumTitle: "Scary Hours", albumCover: "https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180", artist: "Drake", song: "God's Plan", year:"2018" },
-        { id: 2, albumTitle: "More Life", albumCover: "https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180", artist: "Drake, Young Thug", song: "Ice Melts", year:"2017" }
+        { id: 2, albumTitle: "More Life", albumCover: "https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180", artist: "Drake", song: "Fake Love", year:"2017" }
     ]
 }
 
